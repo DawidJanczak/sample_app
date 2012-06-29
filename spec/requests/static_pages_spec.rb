f7ef42af19deed2b27330f62affca7c2933101a7
Dawid Janczak <dawid.janczak@gmail.com>
2012-06-29 13:34:58 +0200

    Remove repetition in specs.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -9,7 +12,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit "/static_pages/home"
-      page.should have_selector("title", :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector("title", :text => "#{base_title} | Home")
     end
   end
 
@@ -21,7 +24,7 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit "/static_pages/help"
-      page.should have_selector("title", :text => "Ruby on Rails Tutorial Sample App | Help")
+      page.should have_selector("title", :text => "#{base_title} | Help")
     end
   end
 
@@ -33,7 +36,7 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit "/static_pages/about"
-      page.should have_selector("title", :text => "Ruby on Rails Tutorial Sample App | About Us")
+      page.should have_selector("title", :text => "#{base_title} | About Us")
     end
   end
 
@@ -45,7 +48,7 @@ describe "Static pages" do
 
     it "should have the title 'Contact'" do
       visit "/static_pages/contact"
-      page.should have_selector("title", :text => "Ruby on Rails Tutorial Sample App | Contact")
+      page.should have_selector("title", :text => "#{base_title} | Contact")
     end
   end
 end
