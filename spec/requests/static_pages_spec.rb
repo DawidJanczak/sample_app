ee9007745d379895e775f4114bfcee777dbf1b86
Dawid Janczak <dawid.janczak@gmail.com>
2012-07-09 13:34:14 +0200

    Change hashes to 1.9 style and remove have_content assertions.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,53 +7,53 @@ describe "Static pages" do
   describe "Home page" do
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_selector("h1", :text => "Sample App")
+      page.should have_selector("h1", text: "Sample App")
     end
 
     it "should have the base title" do
       visit "/static_pages/home"
-      page.should have_selector("title", :text => base_title)
+      page.should have_selector("title", text: base_title)
     end
 
     it "should not have a custom page title" do
       visit "/static_pages/home"
-      page.should_not have_selector("title", :text => "| Home")
+      page.should_not have_selector("title", text: "| Home")
     end
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
+    it "should have the h1 'Help'" do
       visit "/static_pages/help"
-      page.should have_content("Help")
+      page.should have_selector("h1", text: "Help")
     end
 
     it "should have the title 'Help'" do
       visit "/static_pages/help"
-      page.should have_selector("title", :text => "#{base_title} | Help")
+      page.should have_selector("title", text: "#{base_title} | Help")
     end
   end
 
   describe "About page" do
-    it "should have the content 'About Us'" do
+    it "should have the h1 'About Us'" do
       visit "/static_pages/about"
-      page.should have_content("About Us")
+      page.should have_selector("h1", text: "About Us")
     end
 
     it "should have the title 'About Us'" do
       visit "/static_pages/about"
-      page.should have_selector("title", :text => "#{base_title} | About Us")
+      page.should have_selector("title", text: "#{base_title} | About Us")
     end
   end
 
   describe "Contact page" do
-    it "should have the content 'Contact'" do
+    it "should have the h1 'Contact'" do
       visit "/static_pages/contact"
-      page.should have_content("Contact")
+      page.should have_selector("h1", text: "Contact")
     end
 
     it "should have the title 'Contact'" do
       visit "/static_pages/contact"
-      page.should have_selector("title", :text => "#{base_title} | Contact")
+      page.should have_selector("title", text: "#{base_title} | Contact")
     end
   end
 end
