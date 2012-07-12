5b79fca1f0b40fb81b25a411aac03bac6afa51c2
Dawid Janczak <dawid.janczak@gmail.com>
2012-07-12 15:47:33 +0200

    Add email validation.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,4 +35,25 @@ describe User do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
+
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. 
+        foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |address|
+        @user.email = address
+        @user.should_not be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |address|
+        @user.email = address
+        @user.should be_valid
+      end
+    end
+  end
 end
