3d5a5596703525a6138aebabbd9a15365761513a
Dawid Janczak <dawid.janczak@gmail.com>
2012-07-17 12:34:26 +0200

    Change flash to flash.now during incorrect signin.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
   def new
-
   end
 
   def create
@@ -8,7 +7,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Sign the user in and redirect to the user's show page.
     else
-      # Create an error message and re-render the signin form.
+      flash.now[:error] = "Invalid email/password combination"
+      render "new"
     end
   end
 
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -18,6 +18,11 @@ describe "Authentication" do
 
       it { should have_selector("title", text: "Sign in") }
       it { should have_selector("div.alert.alert-error", text: "Invalid") }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector("div.alert.alert-error") }
+      end
     end
 
     describe "with valid information" do
