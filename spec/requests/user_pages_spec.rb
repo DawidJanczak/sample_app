9051350e1170d31c8151ae01e0e3d5999fc8aa3b
Dawid Janczak <dawid.janczak@gmail.com>
2012-07-18 21:33:55 +0200

    Admin can now delete users.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -26,6 +26,24 @@ describe "User pages" do
         end
       end
     end
+
+    describe "delete links" do
+      it { should_not have_link("Delete") }
+
+      describe "as an admin" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link("delete", href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect { click_link("delete") }.to change(User, :count).by(-1)
+        end
+        it { should_not have_link("delete", href: user_path(admin)) }
+      end
+    end
   end
 
   describe "signup page" do
