b7709727c25e51b72c7acd345bbe4a46b67040ab
Dawid Janczak <dawid.janczak@gmail.com>
2012-07-18 15:29:42 +0200

    Add index action for users controller.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -30,9 +30,12 @@ describe "Authentication" do
       before { sign_in(user) }
 
       it { should have_selector("title", text: user.name) }
-      it { should have_link("Profile", href: user_path(user)) }
-      it { should have_link("Sign out", href: signout_path) }
+
+      it { should have_link("Users",    href: users_path) }
+      it { should have_link("Profile",  href: user_path(user)) }
       it { should have_link("Settings", href: edit_user_path(user)) }
+      it { should have_link("Sign out", href: signout_path) }
+
       it { should_not have_link("Sign in", href: signin_path) }
 
       describe "followed by signout" do
@@ -72,6 +75,11 @@ describe "Authentication" do
             page.should have_selector("title", text: "Edit user")
           end
         end
+      end
+
+      describe "visiting the user index" do
+        before { visit users_path }
+        it { should have_selector("title", text: "Sign in") }
       end
     end
 
