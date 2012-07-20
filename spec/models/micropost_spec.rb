dd556985e2a64c9da08cd9e9f92c10287ca19ea6
Dawid Janczak <dawid.janczak@gmail.com>
2012-07-20 15:22:06 +0200

    Generate micropost model and add basic specs.

---
+++ spec/models/micropost_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Micropost do
+  let(:user) { FactoryGirl.create(:user) }
+  before do
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
+  end
+
+  subject(@micropost)
+
+  it { should respond_to(:content) }
+  it { should respond_to(:user_id) }
+end
