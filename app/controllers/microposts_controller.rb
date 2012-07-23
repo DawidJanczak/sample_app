1606391fd505593c4a86af61d68b05a35868b60c
Dawid Janczak <dawid.janczak@gmail.com>
2012-07-23 14:27:53 +0200

    Add new action for user micropost.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,6 +2,13 @@ class MicropostsController < ApplicationController
   before_filter :unsigned_user
 
   def create
+    @micropost = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_path
+    else
+      render "static_pages/home"
+    end
   end
 
   def destroy
