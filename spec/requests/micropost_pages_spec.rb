d714fd2e0e91aa2ffab248c52d22d8bf5e302097
Dawid Janczak <dawid.janczak@gmail.com>
2012-07-24 11:23:44 +0200

    Spec for another user's micropost delete link.
    User should not see delete links on another user's microposts.

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -30,14 +30,25 @@ describe "Micropost pages" do
   end
 
   describe "micropost destruction" do
-    before { FactoryGirl.create(:micropost, user: user) }
-
     describe "as correct user" do
-      before { visit root_path }
+      before do
+        FactoryGirl.create(:micropost, user: user) 
+        visit root_path
+      end
 
       it "should delete a micropost" do
         expect { click_link "delete"}.should change(Micropost, :count).by(-1)
       end
+    end
+
+    describe "as incorrect user" do
+      let(:another_user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: another_user)
+        visit root_path
+      end
+
+      it { should_not have_link("delete") }
     end
   end
 end
